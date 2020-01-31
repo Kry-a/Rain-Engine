9e8bdf4cff0b3ceba5aadf76f8dbe3ce48f2292f
KrasnotR <krasnogorskotr@gmail.com>
2020-01-31 00:43:50 -0300

    Added boilerplate code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Rain-Engine
 An amateurish 3D game engine
+
+```bash
+sudo apt install libglew-dev libglm-dev libglfw3-dev```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+using namespace glm;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    glewExperimental = true;
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Opening a window and it's OpenGL context
+    GLFWwindow* window;
+    window = glfwCreateWindow(800, 450, "Rain Engine", nullptr, nullptr);
+    if (window == nullptr) {
+        fprintf(stderr, "Failed to open GLFW window. Check if you GPU is OpenGL 3.3 compatible.\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        return EXIT_FAILURE;
+    }
+
+    do {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+    return EXIT_SUCCESS;
 }
